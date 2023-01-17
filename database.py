091ae28ba5fc5df36d9a8f9cca0450a3bf4bde3a
Sm0rezdev <simonw0210@gmail.com>
2023-01-17 23:04:41 +0100

    minor improvement

--- database.py
+++ database.py
@@ -1,22 +1,23 @@
 import pickle
 
+
 class Database:
     """
     A class for saving and fetching python objects to and from a file on disk.
     The objects are saved and accessed via a key (of type str).
     """
-    def __init__(self, file_path):
+    def __init__(self, file_path, name='cobalt_db'):
         """
         Initialize the database by loading the data from the file if it exists.
         If the file does not exist, create an empty file.
         """
         self.data = {}
-        self.file_path = file_path
+        self.file_path = f'{file_path}/{name}.pkl'
         try:
-            with open(file_path, "rb") as f:
+            with open(self.file_path, "rb") as f:
                 self.data = pickle.load(f)
         except FileNotFoundError:
-            open(file_path, "wb").close()
+            open(self.file_path, "wb").close()
 
     def save(self, key, obj):
         """Save an object to the database with the given key."""
