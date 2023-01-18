4ba56c2271a8a33b48850bcf3510c6bc515bf3cd
Sm0rezdev <simonw0210@gmail.com>
2023-01-18 01:11:13 +0100

    FIXED: EOFError: Ran out of input

--- database.py
+++ database.py
@@ -6,18 +6,19 @@ class Database:
     A class for saving and fetching python objects to and from a file on disk.
     The objects are saved and accessed via a key (of type str).
     """
-    def __init__(self, file_path, name='cobalt_db'):
+    def __init__(self, file_path):
         """
         Initialize the database by loading the data from the file if it exists.
         If the file does not exist, create an empty file.
         """
         self.data = {}
-        self.file_path = f'{file_path}/{name}.pkl'
+        self.file_path = f'{file_path}_db.pkl'
         try:
             with open(self.file_path, "rb") as f:
                 self.data = pickle.load(f)
         except FileNotFoundError:
-            open(self.file_path, "wb").close()
+            with open(self.file_path, "wb") as f:
+                pickle.dump(self.data, f)
 
     def save(self, key, obj):
         """Save an object to the database with the given key."""
