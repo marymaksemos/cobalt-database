34fdb87ad7fbd61c9a0ba1035ecfa361a515d4a3
marymaksemos <117455640+marymaksemos@users.noreply.github.com>
2023-01-20 22:01:07 +0100

    Improve the  docstrings for the Database class

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import pickle
 
 
 class Database:
-    """
+     """
     A class for saving  python objects to and from a file on disk.
     The objects are saved and accessed via a key (of type str).
     """
@@ -10,6 +10,7 @@ class Database:
         """
         Initialize the database by loading the data from the file if it exists.
         If the file does not exist, create an empty file.
+        :paramter file_path: the path to the file where the data will be stored.
         """
         self.data = {}
         self.file_path = f'{file_path}_db.pkl'
@@ -21,7 +22,11 @@ class Database:
                 pickle.dump(self.data, f)
 
     def save(self, key, obj):
-        """Save an object to the database with the given key."""
+        """
+        Save an object to the database with the given key.
+        :param key: a string representing the key of the object.
+        :param obj: the object to be saved.
+        """
         self.data[key] = obj
         with open(self.file_path, "wb") as f:
             pickle.dump(self.data, f)
